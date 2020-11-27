792b92dedf930433e1751526471f9a583a8cf166
Calvin Chu <calvinchu26@gmail.com>
2020-11-27 20:40:03 +0800

    [story-5][Calvin] feat: SuperSmartParkingBoy park to 2 when 5/10, 3/4, 4/6

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -1,5 +1,6 @@
 package com.oocl.cultivation;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class SuperSmartParkingBoy extends ParkingBoy {
@@ -9,6 +10,7 @@ public class SuperSmartParkingBoy extends ParkingBoy {
 
     @Override
     public ParkingTicket park(Car car) throws NotEnoughPositionException {
-        return super.park(car);
+        ParkingLot parkingLotWithMaxAvailableSpace = this.parkingLots.stream().max(Comparator.comparing(parkingLot -> (double) parkingLot.getAvailableSpace() / parkingLot.getCapacity())).orElse(this.parkingLots.get(0));
+        return parkingLotWithMaxAvailableSpace.park(car);
     }
 }
