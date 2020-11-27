6015e55e6d98281d526c9be07494dffa40375503
Calvin Chu <calvinchu26@gmail.com>
2020-11-27 20:42:39 +0800

    [story-5][Calvin] test: SuperSmartParkingBoy park to 2 when 3/7, 5/5, 5/5

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -35,4 +35,26 @@ public class SuperSmartParkingBoyTest {
         assertEquals(2, smartParkingBoy.getParkingLots().get(1).getAvailableSpace());
         assertEquals(4, smartParkingBoy.getParkingLots().get(2).getAvailableSpace());
     }
+
+    @Test
+    public void should_park_car_in_second_parking_lot_when_park_given_first_available_space_is_3_out_of_7_and_second_available_space_is_5_out_of_5_and_third_available_space_is_5_out_of_5() throws NotEnoughPositionException {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot(7);
+        ParkingLot parkingLot2 = new ParkingLot(5);
+        ParkingLot parkingLot3 = new ParkingLot(5);
+
+        SuperSmartParkingBoy smartParkingBoy = new SuperSmartParkingBoy(Stream.of(parkingLot1, parkingLot2, parkingLot3).collect(Collectors.toList()));
+        parkingLot1.park(new Car());
+        parkingLot1.park(new Car());
+        parkingLot1.park(new Car());
+        parkingLot1.park(new Car());
+
+        //when
+        smartParkingBoy.park(new Car());
+
+        //then
+        assertEquals(3, smartParkingBoy.getParkingLots().get(0).getAvailableSpace());
+        assertEquals(4, smartParkingBoy.getParkingLots().get(1).getAvailableSpace());
+        assertEquals(5, smartParkingBoy.getParkingLots().get(2).getAvailableSpace());
+    }
 }
