c3e131ad6ad199c9944cd0363b389495af536e17
Calvin Chu <calvin.chu@oocl.com>
2020-11-27 15:48:57 +0800

    [parking-lot][Calvin] test: parking boy returns null when park a car and available space

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class ParkingBoyTest {
     @Test
@@ -16,6 +17,19 @@ class ParkingBoyTest {
 
         //then
         assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_return_null_when_park_given_a_car_and_parking_lot_has_no_available_space() {
+        //given
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot(0));
+        Car car = new Car();
+
+        //when
+        ParkingTicket ticket = parkingBoy.park(car);
+
+        //then
+        assertNull(ticket);
     }
 
 }
