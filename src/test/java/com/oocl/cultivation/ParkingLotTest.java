8d08b8d1859d1732a929dfa5e656a21e5cf574bd
Calvin Chu <calvin.chu@oocl.com>
2020-11-27 15:12:15 +0800

    [parking-lot][Calvin] remove: parkingLot tests

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -5,28 +5,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
-    @Test
-    public void should_return_true_when_isVacant_given_available_space() {
-        //given
-        ParkingLot parkingLot = new ParkingLot(1);
-
-        //when
-        Boolean actual = parkingLot.isVacant();
-
-        //then
-        assertTrue(actual);
-    }
-
-    @Test
-    public void should_return_false_when_isVacant_given_no_available_space() {
-        //given
-        ParkingLot parkingLot = new ParkingLot(0);
-
-        //when
-        Boolean actual = parkingLot.isVacant();
-
-        //then
-        assertFalse(actual);
-    }
 
 }
