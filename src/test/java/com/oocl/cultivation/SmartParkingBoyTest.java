f3c4c3cae94fb2a2e704e42ca181b9acc77090d1
Calvin Chu <calvin.chu@oocl.com>
2020-11-27 18:06:15 +0800

    [story-4][Calvin] test: park to lot with max available space

--- src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -27,4 +27,23 @@ public class SmartParkingBoyTest {
         assertEquals(4, smartParkingBoy.getParkingLots().get(2).getAvailableSpace());
     }
 
+    @Test
+    public void should_park_car_in_second_parking_lot_when_park_given_first_available_space_is_3_and_second_available_space_is_5_and_third_available_space_is_5() throws NotEnoughPositionException {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot(3);
+        ParkingLot parkingLot2 = new ParkingLot(5);
+        ParkingLot parkingLot3 = new ParkingLot(5);
+        Car car = new Car();
+
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(Stream.of(parkingLot1, parkingLot2, parkingLot3).collect(Collectors.toList()));
+
+        //when
+        smartParkingBoy.park(car);
+
+        //then
+        assertEquals(3, smartParkingBoy.getParkingLots().get(0).getAvailableSpace());
+        assertEquals(4, smartParkingBoy.getParkingLots().get(1).getAvailableSpace());
+        assertEquals(5, smartParkingBoy.getParkingLots().get(2).getAvailableSpace());
+    }
+
 }
