39bfe51d979c19c5888e7fb5498d0c35e591c083
Calvin Chu <calvin.chu@oocl.com>
2020-11-27 17:38:23 +0800

    [story-3][Calvin] test: multiple parking lots

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,17 +1,19 @@
 package com.oocl.cultivation;
 
-public class ParkingBoy {
-    private ParkingLot parkingLot;
+import java.util.List;
 
-    public ParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
+public class ParkingBoy {
+    private List<ParkingLot> parkingLots;
+
+    public ParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
     }
 
     public ParkingTicket park(Car car) throws NotEnoughPositionException {
-        return this.parkingLot.park(car);
+        return this.parkingLots.get(0).park(car);
     }
 
     public Car fetch(ParkingTicket ticket) throws UnrecognizedParkingTicketException {
-        return this.parkingLot.fetch(ticket);
+        return this.parkingLots.get(0).fetch(ticket);
     }
 }
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -3,6 +3,11 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -11,7 +16,7 @@ class ParkingBoyTest {
     void should_call_parking_lot_fetch_when_fetch() throws UnrecognizedParkingTicketException, NotEnoughPositionException {
         //given
         ParkingLot parkingLotMock = Mockito.mock(ParkingLot.class);
-        ParkingBoy parkingBoy = new ParkingBoy(parkingLotMock);
+        ParkingBoy parkingBoy = new ParkingBoy(Stream.of(parkingLotMock).collect(Collectors.toList()));
         Car car = new Car();
         ParkingTicket parckingTicket = parkingBoy.park(car);
         //when
@@ -25,7 +30,7 @@ class ParkingBoyTest {
     void should_call_parking_lot_park_when_park() throws NotEnoughPositionException {
         //given
         ParkingLot parkingLotMock = Mockito.mock(ParkingLot.class);
-        ParkingBoy parkingBoy = new ParkingBoy(parkingLotMock);
+        ParkingBoy parkingBoy = new ParkingBoy(Stream.of(parkingLotMock).collect(Collectors.toList()));
         Car car = new Car();
         //when
         parkingBoy.park(car);
@@ -33,5 +38,21 @@ class ParkingBoyTest {
         //then
         verify(parkingLotMock, times(1)).park(car);
     }
+
+    @Test
+    public void should_park_to_second_parking_low_when_park_given_first_parking_lot_is_full_and_second_is_vacant() throws NotEnoughPositionException {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot(0);
+        ParkingLot parkingLot2 = new ParkingLot(3);
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(Stream.of(parkingLot1, parkingLot2).collect(Collectors.toList()));
+
+        //when
+        ParkingTicket ticket = parkingBoy.park(car);
+
+        //then
+        assertNotNull(ticket);
+    }
+
 
 }
