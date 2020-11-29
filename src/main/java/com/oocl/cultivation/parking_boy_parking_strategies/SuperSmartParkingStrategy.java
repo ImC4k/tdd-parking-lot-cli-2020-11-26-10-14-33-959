9b9778603d7b40c6e706eb9a66e34e3b3f90611a
Calvin Chu <calvinchu26@gmail.com>
2020-11-29 10:19:23 +0800

    [parking-lot][Calvin] refactor: super smart parking strategy

--- src/main/java/com/oocl/cultivation/parking_boy_parking_strategies/SuperSmartParkingStrategy.java
+++ src/main/java/com/oocl/cultivation/parking_boy_parking_strategies/SuperSmartParkingStrategy.java
@@ -11,8 +11,7 @@ import java.util.List;
 public class SuperSmartParkingStrategy implements ParkingBoyParkingStrategy {
     @Override
     public ParkingTicket park(Car car, List<ParkingLot> parkingLots) throws NotEnoughPositionException {
-        ParkingLot parkingLotWithMaxAvailableSpace = parkingLots.stream().max(Comparator.comparing(this::getAvailablePositionRate)).orElse(parkingLots.get(0));
-        return parkingLotWithMaxAvailableSpace.park(car);
+        return parkingLots.stream().max(Comparator.comparing(this::getAvailablePositionRate)).orElseThrow(NotEnoughPositionException::new).park(car);
     }
 
 
